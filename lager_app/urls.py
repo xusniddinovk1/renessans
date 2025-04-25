af9ea51dcac90a028dee4987a64ee0b9067b3b5e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-04-25 11:17:42 +0500

    initial commit

--- lager_app/urls.py
+++ lager_app/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from lager_app.views import activity_page
+from lager_app.views import activity_page, home_page
 
 urlpatterns = [
+    path('', home_page, name='home_page'),
     path('faoliyat/', activity_page, name='activity_page'),
+    path('oquvbolim/', activity_page, name='education_page'),
+    path('mexmonxonalar/', activity_page, name='hotel_section_page'),
+    path('yangiliklar/', activity_page, name='news_page'),
+    path('istirohatzona/', activity_page, name='recreation_zone_page'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- lager_app/views.py
+++ lager_app/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, redirect
 from .models import *
 
 
+def home_page(request):
+    photos = Photos.objects.all()
+    ctx = {
+        "photos": photos
+    }
+    return render(request, 'lager/index.html', ctx)
+
+
 def activity_page(request):
     activities = Activity.objects.all()
     ctx = {
         "activities": activities
     }
     return render(request, 'lager/faoliyat.html', ctx)
-
-
-
-
-
