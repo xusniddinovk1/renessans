b57e8a68f560e56646c9320f63992385de62a66d
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-04 11:01:52 +0500

    initial commit

--- lager_app/models.py
+++ lager_app/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 
 class AboutUs(models.Model):
-    context_uz = models.TextField()
-    context_ru = models.TextField()
-    context_en = models.TextField()
+    context = models.TextField()
 
     def __str__(self):
-        return self.context_uz[:50]
+        return self.context[:50]
 
 
 class Photos(models.Model):
@@ -15,16 +13,12 @@ class Photos(models.Model):
 
 
 class Activity(models.Model):
-    title_uz = models.CharField(max_length=100, null=False, blank=False)
-    title_ru = models.CharField(max_length=100, null=False, blank=False)
-    title_en = models.CharField(max_length=100, null=False, blank=False)
-    description_uz = models.TextField()
-    description_ru = models.TextField()
-    description_en = models.TextField()
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
     image = models.ImageField(upload_to='activities/', blank=True, null=True)
 
     def __str__(self):
-        return self.title_uz[:30]
+        return self.title
 
 
 class Hotel(models.Model):
@@ -37,37 +31,25 @@ class Hotel(models.Model):
 
 
 class Education(models.Model):
-    title_uz = models.CharField(max_length=100, null=False, blank=False)
-    title_ru = models.CharField(max_length=100, null=False, blank=False)
-    title_en = models.CharField(max_length=100, null=False, blank=False)
-    description_uz = models.TextField()
-    description_ru = models.TextField()
-    description_en = models.TextField()
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
     image = models.ImageField(upload_to='educations/', blank=True, null=True)
 
 
 class RecreationZone(models.Model):
-    title_uz = models.CharField(max_length=100, null=False, blank=False)
-    title_ru = models.CharField(max_length=100, null=False, blank=False)
-    title_en = models.CharField(max_length=100, null=False, blank=False)
-    description_uz = models.TextField()
-    description_ru = models.TextField()
-    description_en = models.TextField()
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField()
     image = models.ImageField(upload_to='recreation/', blank=True, null=True)
 
     def __str__(self):
-        return self.title_uz
+        return self.title
 
 
 class News(models.Model):
-    title_uz = models.CharField(max_length=100, null=False, blank=False)
-    title_ru = models.CharField(max_length=100, null=False, blank=False)
-    title_en = models.CharField(max_length=100, null=False, blank=False)
-    content_uz = models.TextField()
-    content_ru = models.TextField()
-    content_en = models.TextField()
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField()
     image = models.ImageField(upload_to='news/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title_uz
+        return self.title
