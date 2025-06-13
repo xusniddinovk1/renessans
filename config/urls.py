4a95ee16474fa8b6c3395e884b7b3701e77602bf
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-13 13:51:45 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('lager_app.urls')),
-    path('dashboard/', include('dashboard.urls'))
+    path('admin/', include('dashboard.urls'))
 ]
 
 if settings.DEBUG:
