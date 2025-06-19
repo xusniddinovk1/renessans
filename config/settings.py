a176e948f3094dc00a9be76b14fcd93dc61041cf
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-06-19 16:38:33 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
